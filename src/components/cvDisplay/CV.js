c68f4f4fb4f8a3376311eb78d9854613847f0d01
MateoLCS <mateusz.namysl0318@gmail.com>
2022-09-04 16:33:03 +0200

    finished cv layout

--- src/components/cvDisplay/CV.js
+++ src/components/cvDisplay/CV.js
@@ -10,10 +10,15 @@ const CV = ({ cv }) => {
         <h4 className="cv__section__title">
           {cv.workExperience[i].companyName}
         </h4>
-        <p>
-          {cv.workExperience[i].from} - {cv.workExperience[i].to}
-        </p>
-        <p>Position: {cv.workExperience[i].position}</p>
+        <div className="cv__section__content">
+          <div className="cv__section__dates">
+            <p>From: {cv.workExperience[i].from}</p>
+            <p>To: {cv.workExperience[i].to}</p>
+          </div>
+          <p className="cv__middle">
+            Position: {cv.workExperience[i].position}
+          </p>
+        </div>
       </div>
     );
   }
@@ -22,10 +27,13 @@ const CV = ({ cv }) => {
     educationInfo.push(
       <div key={i} className="cv__section">
         <h4 className="cv__section__title">{cv.educationInfo[i].schoolName}</h4>
-        <p>
-          {cv.educationInfo[i].from} - {cv.educationInfo[i].to}
-        </p>
-        <p>Degree: {cv.educationInfo[i].degree}</p>
+        <div className="cv__section__content">
+          <div className="cv__section__dates">
+            <p>From: {cv.educationInfo[i].from}</p>
+            <p>To: {cv.educationInfo[i].to}</p>
+          </div>
+          <p className="cv__middle">Degree: {cv.educationInfo[i].degree}</p>
+        </div>
       </div>
     );
   }
@@ -35,10 +43,34 @@ const CV = ({ cv }) => {
       <div className="cv__header"></div>
       <div className="cv__main">
         <div className="cv__content__container">
-          {workExperience}
-          {educationInfo}
+          <div className="cv__element border__bottom">
+            <h3 className="cv__element__title">Work Experience</h3>
+            {workExperience}
+          </div>
+          <div className="cv__element">
+            <h3 className="cv__element__title">Education</h3>
+            {educationInfo}
+          </div>
         </div>
-        <div className="cv__side__content"></div>
+        <div className="cv__side__content">
+          <div className="cv__side__content__container">
+            <p className="cv__side__content__item">
+              {cv.personalInfo.name} {cv.personalInfo.surname}
+            </p>
+            <p className="cv__side__content__item">
+              {cv.personalInfo.birthDate}
+            </p>
+            <p className="cv__side__content__item">
+              Phone number: {cv.personalInfo.phoneNumber}
+            </p>
+            <p className="cv__side__content__item">
+              Email: {cv.personalInfo.email}
+            </p>
+            <p className="cv__side__content__item">
+              Address: {cv.personalInfo.address}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
